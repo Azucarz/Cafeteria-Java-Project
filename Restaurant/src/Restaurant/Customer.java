99af111a20d181685a41aa732c15fa4491d00411
zachwong02 <wongzhizhen3@gmail.com>
2022-08-14 18:29:59 +0800

    Write to file function (INCOMPLETE)

--- Restaurant/src/Restaurant/Customer.java
+++ Restaurant/src/Restaurant/Customer.java
@@ -1,5 +1,7 @@
 package Restaurant;
 
 public class Customer extends User{
-
+    public Customer(String name, String password, String email, String number) {
+        super(name, password, email, number);
+    }
 }
--- Restaurant/src/Restaurant/Manager.java
+++ Restaurant/src/Restaurant/Manager.java
@@ -1,5 +1,7 @@
 package Restaurant;
 
 public class Manager extends User{
-
+    public Manager(String name, String password, String email, String number) {
+        super(name, password, email, number);
+    }
 }
