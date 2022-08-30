27d21df0f7fb36757959ea8a3439d2b07aa6f936
zachwong02 <wongzhizhen3@gmail.com>
2022-08-30 21:59:12 +0800

    Add to Cart function

---
+++ Restaurant/src/Restaurant/CartUI.java
@@ -0,0 +1,16 @@
+package Restaurant;
+
+import javax.swing.*;
+
+public class CartUI extends UI{
+//    View total items in the cart
+//    Receipt format
+//    Amount of the items are buttons
+//    button will open up ItemUI (Same logic)
+
+    private JFrame frame;
+    private JPanel panel;
+
+    public CartUI(Customer c) {
+    }
+}
